9bfa77a5b1568c99134a5a8700a1aa733a46b091
Zizo95 <147083219+Zizo95@users.noreply.github.com>
2023-10-16 17:33:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ function displayWeatherCondition(response) {
 function searchCity(city) {
   let apiKey = "cabdbda40038ba7d1165b953b1c7bd6c";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeatherCondition);
+  axios.get(apiUrl).then(displayTemperature);
   displayTemperature(response);
 }
 
